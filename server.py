1123561be68f8e0035fb045c0aa9ccdd8a6c67d6
YashviSoni-44 <142931859+YashviSoni-44@users.noreply.github.com>
2025-07-29 13:20:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -72,4 +72,6 @@ def static_proxy(path):
         return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
+
